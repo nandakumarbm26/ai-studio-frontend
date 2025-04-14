7879a194b4ffd513bccb501a29420af7c01ef7c1
nandakumarbm26 <nanda262001@gmail.com>
2025-04-14 16:36:48 +0530

    domain fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["ai-studio-frontend-zeta.vercel.app"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
